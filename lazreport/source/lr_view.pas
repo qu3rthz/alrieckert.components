qu3rthz/alrieckert.components

commit 111fbf48496018ddc072d38ca95da1a2909cc86a

lazreport: less notes
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@36420 4005530d-fff6-0310-9dd1-cebe43e6787f

lazreport/source/lr_view.pas
@@ -153,8 +153,8 @@ type
     procedure DelPageBtnClick(Sender: TObject);
     procedure NewPageBtnClick(Sender: TObject);
     procedure HelpBtnClick(Sender: TObject);
-    procedure FormMouseDown(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: Integer);
+    procedure FormMouseDown(Sender: TObject; {%H-}Button: TMouseButton;
+      {%H-}Shift: TShiftState; {%H-}X, {%H-}Y: Integer);
     procedure FormActivate(Sender: TObject);
     procedure FormDeactivate(Sender: TObject);
   private
@@ -169,8 +169,6 @@ type
     PaintAllowed: Boolean;
     FindStr: String;
     CaseSensitive: Boolean;
-    StrFound: Boolean;
-    StrBounds: TRect;
     LastFoundPage, LastFoundObject: Integer;
     HF: String;
     
@@ -918,17 +916,19 @@ begin
 end;
 
 procedure TfrPreviewForm.HScrollBarChange(Sender: TObject);
+{$IFDEF WIN32}
 var
   p, pp: Integer;
   r: TRect;
+{$ENDIF}
 begin
   if EMFPages = nil then Exit;
+  {$IFDEF WIN32}
   p := HScrollBar.Position;
   pp := OldH - p;
   OldH := p;
   ofx := -p;
   r := Rect(0, 0, PBox.Width, PBox.Height);
-  {$IFDEF WIN32}
   ScrollWindowEx(PBox.Handle, pp, 0, @r, @r, 0, nil, SW_INVALIDATE);
   UpdateWindow(Pbox.Handle);
   {$ELSE}
@@ -1282,8 +1282,6 @@ end;
 
 procedure TfrPreviewForm.FormMouseDown(Sender: TObject;
   Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
-var
-  c: TControl;
 begin
 //  HelpBtn.Down := False;
   Screen.Cursor := crDefault;

commit 4e8d41ba2e75dee7930a5a140a8488438d89d767

LazReport, fix preview scrolling broken in r36420, issue 21633
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@36887 4005530d-fff6-0310-9dd1-cebe43e6787f

lazreport/source/lr_view.pas
@@ -888,18 +888,19 @@ end;
 procedure TfrPreviewForm.VScrollBarChange(Sender: TObject);
 var
   {$IFDEF WIN32}
-  p, pp: Integer;
   r: TRect;
+  pp: Integer;
   {$ENDIF}
+  p: Integer;
   i: integer;
   Pages: TfrEMFPages;
 begin
   if EMFPages = nil then Exit;
-  {$IFDEF WIN32}
   p := VScrollBar.Position;
-  pp := OldV - p;
-  OldV := p;
   ofy := -p;
+  {$IFDEF WIN32}
+  OldV := p;
+  pp := OldV - p;
   r := Rect(0, 0, PBox.Width, PBox.Height);
   ScrollWindowEx(PBox.Handle, 0, pp, @r, @r, 0, nil, SW_INVALIDATE);
   UpdateWindow(Pbox.Handle);
@@ -918,18 +919,19 @@ begin
 end;
 
 procedure TfrPreviewForm.HScrollBarChange(Sender: TObject);
-{$IFDEF WIN32}
 var
-  p, pp: Integer;
+  p: Integer;
+{$IFDEF WIN32}
+  pp: Integer;
   r: TRect;
 {$ENDIF}
 begin
   if EMFPages = nil then Exit;
-  {$IFDEF WIN32}
   p := HScrollBar.Position;
+  ofx := -p;
+  {$IFDEF WIN32}
   pp := OldH - p;
   OldH := p;
-  ofx := -p;
   r := Rect(0, 0, PBox.Width, PBox.Height);
   ScrollWindowEx(PBox.Handle, pp, 0, @r, @r, 0, nil, SW_INVALIDATE);
   UpdateWindow(Pbox.Handle);